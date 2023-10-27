83d0d45ac5b99e52c178a2b9b94543679dc160f3
miroslaw-mroczkowski <mroczkowski.miroslaw@gmail.com>
2023-10-27 14:36:22 +0200

    mobile and tablet ui finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaulTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	content: ['./index.html', './src/**/*.{html,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				chivo: ['Chivo', ...defaulTheme.fontFamily.sans],
+			},
+			colors: {
+				customTextGreen: '#54e6af',
+				customTextGreenHover: '#b3ffe2',
+				customTextLightGray: '#c2cbe5',
+				customBgDark: '#121725',
+				customBgLight: '#2c344b',
+				customBgIcons: '#5a668a',
+				customError: '#FB3E3E',
+			},
+		},
 	},
 	plugins: [],
 };
